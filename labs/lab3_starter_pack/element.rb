0572f39ec4d9fbfd6458d48ea5b4464e6806898b
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-16 13:51:47 -0800

    added tests, fixed abstract method syntax error

--- labs/lab3_starter_pack/element.rb
+++ labs/lab3_starter_pack/element.rb
@@ -6,7 +6,7 @@ class Element
     @name = name
   end
 
-  undef compare_to(other)
+  def compare_to(other)
     fail "This method must be overridden in a subclass"
   end
 end
@@ -19,15 +19,15 @@ class Rock < Element
   def compare_to(other)
     case other
     when Rock
-      # return ['self.name equals other.name', 'Tie']
+      ['Rock equals Rock', 'Tie']
     when Paper
-      # return ['other.name covers self.name', 'Lose']
+      ['Paper covers Rock', 'Lose']
     when Scissors
-      # return ['self.name crushes other.name', 'Win']
+      ['Rock crushes Scissors', 'Win']
     when Lizard
-      # return ['Rock crushes Lizard', 'Win']
+      ['Rock crushes Lizard', 'Win']
     when Spock
-      # return ['Spock vaporizes Rock', 'Lose']
+      ['Spock vaporizes Rock', 'Lose']
     end
   end
 
@@ -39,15 +39,15 @@ class Paper < Element
   def compare_to(other)
     case other
     when Rock
-      # return ['Paper covers Rock', 'Win']
+      ['Paper covers Rock', 'Win']
     when Paper
-      # return ['Paper equals Paper', 'Tie']
+      ['Paper equals Paper', 'Tie']
     when Scissors
-      # return ['Scissors cuts Paper', 'Lose']
+      ['Scissors cuts Paper', 'Lose']
     when Lizard
-      # return ['Lizard eats Paper', 'Lose']
+      ['Lizard eats Paper', 'Lose']
     when Spock
-      # return ['Paper disproves Spock', 'Win']
+      ['Paper disproves Spock', 'Win']
     end
   end
 
@@ -59,15 +59,15 @@ class Scissors < Element
   def compare_to(other)
     case other
     when Rock
-      # return ['Rock crushes Scissors', 'Lose']
+      ['Rock crushes Scissors', 'Lose']
     when Paper
-      # return ['Scissors cuts Paper', 'Win']
+      ['Scissors cuts Paper', 'Win']
     when Scissors
-      # return ['Scissors equals Scissors', 'Tie']
+      ['Scissors equals Scissors', 'Tie']
     when Lizard
-      # return ['Scissors decapitates Lizard', 'Win']
+      ['Scissors decapitates Lizard', 'Win']
     when Spock
-      # return ['Spock smashes Scissors', 'Lose']
+      ['Spock smashes Scissors', 'Lose']
     end
   end
 
@@ -79,15 +79,15 @@ class Lizard < Element
   def compare_to(other)
     case other
     when Rock
-      # return ['Rock crushes Lizard', 'Lose']
+      ['Rock crushes Lizard', 'Lose']
     when Paper
-      # return ['Lizard eats Paper', 'Win']
+      ['Lizard eats Paper', 'Win']
     when Scissors
-      # return ['Scissors decapitates Lizard', 'Lose']
+      ['Scissors decapitates Lizard', 'Lose']
     when Lizard
-      # return ['Lizard equals Lizard', 'Tie']
+      ['Lizard equals Lizard', 'Tie']
     when Spock
-      # return ['Lizard poisons Spock', 'Win']
+      ['Lizard poisons Spock', 'Win']
     end
   end
 
@@ -99,16 +99,24 @@ class Spock < Element
   def compare_to(other)
     case other
     when Rock
-      # return ['Spock vaporizes Rock', 'Win']
+      ['Spock vaporizes Rock', 'Win']
     when Paper
-      # return ['Paper disproves Spock', 'Lose']
+      ['Paper disproves Spock', 'Lose']
     when Scissors
-      # return ['Spock crushes Scissors', 'Win']
+      ['Spock crushes Scissors', 'Win']
     when Lizard
-      # return ['Lizard poisons Spock', 'Lose']
+      ['Lizard poisons Spock', 'Lose']
     when Spock
-      # return ['Spock equals Spock', 'Tie']
+      ['Spock equals Spock', 'Tie']
     end
   end
 
 end
+
+
+# Tests
+rock  = Rock.new('Rock')
+paper = Paper.new('Paper')
+puts rock.compare_to(paper)
+puts paper.compare_to(rock)
+puts rock.compare_to(rock)

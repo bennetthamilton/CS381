13ede481dda56d308285a7106848341a42d0641e
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-16 13:05:15 -0800

    started setting up element class and subclasses

--- labs/lab3_starter_pack/element.rb
+++ labs/lab3_starter_pack/element.rb
@@ -1,3 +1,45 @@
-class Element
 
+class Element
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  undef compare_to(other)
+    fail "This method must be overridden in a subclass"
+  end
+end
+
+
+# Subclasses
+
+class Rock < Element
+  def compare_to(other)
+    # TODO
+  end
+end
+
+class Paper < Element
+  def compare_to(other)
+    # TODO
+  end
+end
+
+class Scissors < Element
+  def compare_to(other)
+    # TODO
+  end
+end
+
+class Lizard < Element
+  def compare_to(other)
+    # TODO
+  end
+end
+
+class Spock < Element
+  def compare_to(other)
+    # TODO
+  end
 end

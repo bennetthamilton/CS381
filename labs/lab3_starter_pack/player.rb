72c5476b8c799407f2cff22820906042a6128bb8
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-16 14:57:22 -0800

    added randombot with own play method

--- labs/lab3_starter_pack/player.rb
+++ labs/lab3_starter_pack/player.rb
@@ -20,5 +20,16 @@ class StupidBot < Player
   def play
     rock = Rock.new("Rock")
     @history.log_play(rock)
+    rock
+  end
+end
+
+class RandomBot < Player
+  def play
+    # ref: https://www.geeksforgeeks.org/ruby-array-sample-function/
+    element_array = [Rock.new("Rock"), Paper.new("Paper"), Scissors.new("Scissors"), Lizard.new("Lizard"), Spock.new("Spock")]
+    random = element_array.sample          
+    @history.log_play(random)
+    random
   end
 end

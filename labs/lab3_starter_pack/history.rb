98ff50c7f5ddac39e8623734c5faceb9349e8e13
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-16 14:26:29 -0800

    added instance vars and methods to history class

--- labs/lab3_starter_pack/history.rb
+++ labs/lab3_starter_pack/history.rb
@@ -1,3 +1,22 @@
-class History
 
+class History
+  attr_accessor :plays, :opponent_plays, :score
+
+  def initialize
+    @plays = Array.new
+    @opponent_plays = Array.new
+    @score = 0
+  end
+
+  def log_play(move)
+    # TODO
+  end
+
+  def log_opponent_play(move)
+    # TODO
+  end
+
+  def add_score()
+    @score += 1
+  end
 end

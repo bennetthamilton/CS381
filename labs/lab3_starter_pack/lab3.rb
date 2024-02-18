442f983ff5622827811ad0df458ef9595a822e01
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-18 15:57:25 -0800

    turned into game class, planning out methods

--- labs/lab3_starter_pack/lab3.rb
+++ labs/lab3_starter_pack/lab3.rb
@@ -13,15 +13,92 @@ require_relative "history"   # uncomment to load history.rb
 # constants
 ROUNDS = 5
 
-# display the welcome message
-def welcome
-	puts "Welcome to Rock, Paper, Scissors, Lizard, Spock!"
-end
+class Game
 
-# main function
-def game(rounds)
+	def initialize(@rounds)
+		@rounds = rounds
+		@players = []
+		@history = History.new
+	end
 
-end
+	# select players
+	def select_players
+		# display player options
+		# selcect players loop
+			# get player1 index
+			# get player2 index
+			# if valid players
+				# create player1
+				# create player2
+			# else
+				# display error message
+		# end
+	end
 
-welcome				# call to display the welcome message
-game(ROUNDS)	# call to kick off the game
+	# play a single round
+	def play_round
+		# get player1 move
+		# get player2 move
+		# compare moves
+		# display winner
+		# update history
+		# update rounds
+		# display winner of the round
+	end
+
+	# main function
+	def play_game
+		while rounds > 0
+			play_round
+		end
+		display_results
+		display_winner_of(winner, "game")
+	end
+
+	private
+
+	# display the welcome message
+	def welcome
+		puts "Welcome to Rock, Paper, Scissors, Lizard, Spock!"
+	end
+
+	# display player options
+	def display_player_options
+
+	end
+
+	# get player input
+	def get_player_index(player_number)
+		# puts Select player (player_number)
+		# gets.chomp.to_i
+	end
+
+	# checks if player input is valid
+	def valid_player?(player_index)
+		# valid if player_index is between 1 and 5
+	end
+
+	# create a player
+	def create_player(player_index)
+		# create new player object
+	end
+
+	# checks if move input is valid
+	def valid_move?(move_index)
+		# valid if move_index is between 1 and 5
+	end
+
+	# display final score results
+	def display_results
+		# puts final score is blank to blank
+	end
+
+	# display the winner of the round
+	def display_winner_of(winner, type)
+		# puts winning player is the winner of the round/game!
+	end
+end 
+
+# main play game call
+game = Game.new(ROUNDS)
+game.play

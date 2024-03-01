3faccb4ca5e471e31b929c423075b45998015b89
bennetthamilton <bennetthamilton0@gmail.com>
2024-03-01 10:05:10 -0800

    implemented member function

--- labs/381_lab4_starter_pack/lab4.rkt
+++ labs/381_lab4_starter_pack/lab4.rkt
@@ -2,16 +2,42 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;     CS 381 - Programming Lab #4		;
 ;										;
-;  < your name >					 	;
-;  < your email >	                	;
+;  < Bennett Hamilton >					 	;
+;  < hamibenn@oregonstate.edu >	                	;
 ;										;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-; your code here
+(define (member? e lst)
+  ; Check if the list is empty
+  (if (null? lst)
+      #f                         ; if the list is empty, e is not a member, return false
+      (if (eq? e (car lst))
+          #t                     ; if e is equal to the first element, it's a member 
+          (member? e (cdr lst))  ; otherwise, iterate the rest of the list
+      )
+  )
+) 
 
+(define (set? lst) 
+  ; complete this function definition 
+)
 
+(define (union lst1 lst2)
+  ; complete this function definition
+)
 
+(define (intersect lst1 lst2)
+  ; complete this function definition
+)
+
+(define (difference lst1 lst2)
+  ; complete this function definition
+)
+
+;;; (define (flatten lst1 lst2)
+;;;   ; complete this function definition
+;;; )
 
 ;;;;;;;;;;;;;;;;;
 ;  DO NOT EDIT  ;
